d3e5e7d823ef37cade5ee33654ce01cc11312dd9
quatroxx <serhatha@gmail.com>
2025-04-26 22:43:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ function toggleMenu() {
   var menu = document.getElementById("mobileMenu");
   var body = document.querySelector('main');
   menu.classList.toggle("show-menu");
-  body.classList.toggle("blurred");
 }
 
 document.addEventListener('click', function(event) {
   const menu = document.getElementById("mobileMenu");
   const toggle = document.querySelector(".hamburger");
   const closeBtn = document.querySelector(".close-btn");
+  const body = document.querySelector('main');
 
-  // Eğer menü açıksa ve tıklanan yer menünün içinde DEĞİLSE ve hamburger/çarpı değilse
   if (
     menu.classList.contains("show-menu") &&
     !menu.contains(event.target) &&
@@ -18,8 +17,7 @@ document.addEventListener('click', function(event) {
     !(closeBtn && closeBtn.contains(event.target))
   ) {
     menu.classList.remove("show-menu");
-body.classList.remove("blurred");
-
+    body.classList.remove("blurred");
   }
 });
 document.addEventListener('DOMContentLoaded', function() {
